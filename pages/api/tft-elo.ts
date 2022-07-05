a5dd974065e791e64a6a89e5c864008358c87362
Vinicius Farias Bastos <vinicius.bastos@basis.com.br>
2022-07-05 14:48:09 -0300

    Update to LegendsAPI

--- pages/api/tft-elo.ts
+++ pages/api/tft-elo.ts
@@ -3,32 +3,6 @@ import type {NextApiRequest, NextApiResponse} from 'next'
 import fetch from 'node-fetch';
 import {getApiData} from "../../public/handler/api-fetch-tft";
 
-function validaDadosPreenchidos(user: string | string[], region: string | string[]) {
-    let userFinal = user.toString();
-    let regionFinal = region.toString().toUpperCase();
-
-    const regionList = [
-        'BR',
-        'NA'
-    ];
-
-    if (!userFinal) {
-        throw new Error('User param is missing');
-    }
-
-    if (!regionFinal || !regionList.includes(regionFinal)) {
-        throw new Error(`Region param is missing, available regions: ${regionList}`);
-    }
-
-    switch (regionFinal) {
-        default:
-            regionFinal = 'br1';
-            break;
-    }
-
-    return {userFinal, regionFinal};
-}
-
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
@@ -54,7 +28,7 @@ export default async function handler(
 
         const finalPlacementString = !!disableLastPositions === true ? '' : ` Last Positions: [${placementString}]`;
 
-        res.setHeader('Cache-Control', 's-maxage=180, stale-while-revalidate');
+        res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
 
         res.status(200).json(`${user}: ${tier} ${apiData.playerInfo.rank} (${lp})${finalPlacementString}`);
 
